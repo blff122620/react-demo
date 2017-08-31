447ac0ded3bb739a5ab1c46c35cb1afc952c6ad5
blff122620 <blff122620@gmail.com>
2017-08-31 11:52:54 +0800

    test

---
+++ src/temp/Mtest.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Button from 'material-ui/Button';
+
+class Mtest extends React.Component{
+  render(){
+    return (
+      <div>
+        <Button>
+          Hello World
+        </Button>
+      </div>
+    );
+  }
+}
+
+export default Mtest;
